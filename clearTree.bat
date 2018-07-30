8d3e017f3efbef8299f1fc2c58ce6c1f4228f510
Dzianis Khamutouski <denishomutovski@gmail.com>
2018-07-30 08:27:18 +0200

    trying ctest CI

---
+++ clearTree.bat
@@ -0,0 +1,5 @@
+rm -r *.elf *.a CMakeCache.txt ^
+CMakeFiles cmake_install.cmake Makefile ^
+source\CMakeFiles source\cmake_install.cmake source\Makefile ^
+libs\CMakeFiles libs\cmake_install.cmake libs\Makefile ^
+unitTests\CMakeFiles unitTests\cmake_install.cmake unitTests\Makefile
