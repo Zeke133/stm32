046371feb7ba1d1ec721640636cf37152a02530e
Dzianis Khamutouski <denishomutovski@gmail.com>
2018-09-09 17:01:12 +0200

    little changes

--- generateBuildTest.bat
+++ generateBuildTest.bat
@@ -2,4 +2,3 @@ ctest   --build-and-test . build ^
         --build-generator "Unix Makefiles" ^
         --build-options -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake ^
         --test-command ctest -j 4
-        
---
+++ generateBuildTest.sh
@@ -0,0 +1,4 @@
+ctest   --build-and-test . build \
+        --build-generator "Unix Makefiles" \
+        --build-options -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake \
+        --test-command ctest -j 4
