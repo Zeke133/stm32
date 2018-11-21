b43b6d287b5ea3e3e2fcdd656a5a785ca48c3a01
Denis Homutovski <denishomutovski@gmail.com>
2018-11-21 18:18:20 +0100

    scripts for work on mac added

---
+++ scripts/nix/bootloaderRun.sh
@@ -0,0 +1,2 @@
+# Start execution:
+../../../stm32flash/stm32flash -b 230400 -g 0x0 /dev/tty.PL2303-1B1
---
+++ scripts/nix/bootloaderWrite.sh
@@ -0,0 +1,5 @@
+# Write with verify
+../../../stm32flash/stm32flash\
+    -b 230400\
+    -w /Users/dzianiskhamutouski/Developement/stm32/bin/app.hex\
+    -v /dev/tty.PL2303-1B1
